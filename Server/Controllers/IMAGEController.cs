3999d392ae73e29da1e4bb4e0da35f2225485aec
Anil Roychoudhry <anil@roychoudhry.com>
2019-12-29 18:52:43 +1100

    added camera capture logic

--- Server/Controllers/IMAGEController.cs
+++ Server/Controllers/IMAGEController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
 using System.Device.Gpio;
 using garagedoor.Shared;
+using MMALSharp;
+using MMALSharp.Common.Utility;
+using MMALSharp.Handlers;
+using MMALSharp.Native;
 
 namespace garagedoor.Server.Controllers
 {
@@ -16,17 +20,31 @@ namespace garagedoor.Server.Controllers
     public class IMAGEController : ControllerBase
     {
         private readonly ILogger<IMAGEController> logger;
+        private MMALCamera cam;
         
         public IMAGEController(
             ILogger<IMAGEController> logger)
         {
             this.logger = logger;
+            this.cam = MMALCamera.Instance;
+            MMALCameraConfig.StillResolution = new Resolution(640, 480); 
         }
 
         [HttpPost]
-        public void Post([FromBody] bool refresh)
+        public async Task<ContentResult> Post([FromBody] bool refresh)
+        //public ContentResult Post([FromBody] bool refresh)
         {            
+            String fileName="NotAnImage.jpg";
             logger.LogInformation("Will refresh Image");
+
+            using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))        
+            {            
+                await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+                fileName = imgCaptureHandler.GetFilename(); 
+                logger.LogInformation(fileName);
+            }
+            return Content(fileName);
+
         }
 
     }
